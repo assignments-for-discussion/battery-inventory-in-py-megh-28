aaec975c0fc10ab8dfc992765a867727bd8c789c
megh-28 <89695503+megh-28@users.noreply.github.com>
2023-12-03 17:18:52 +0530

    Added comments

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ def count_batteries_by_health(present_capacities):
         "failed": 0
     }
 
+    # Iterate through each battery's present capacity
     for capacity in present_capacities:
+        # Define the rated capacity of a new battery
         rated_capacity = 120
+        # Calculate the State-of-Health (SoH) percentage
         soh_percentage = (capacity / rated_capacity) * 100
 
+        # Classify batteries based on SoH and update counts
         if soh_percentage > 80:
             counts["healthy"] += 1
         elif 62 <= soh_percentage <= 80:
@@ -16,6 +20,7 @@ def count_batteries_by_health(present_capacities):
         else:
             counts["failed"] += 1
 
+    # Return the counts for each health category
     return counts
 
 
@@ -23,6 +28,10 @@ def test_bucketing_by_health():
     print("Counting batteries by SoH...\n")
     present_capacities = [113, 116, 80, 95, 92, 70]
     counts = count_batteries_by_health(present_capacities)
+    print("Health Conditions of a batteries")
+    print("Healthy battery:", counts["healthy"])
+    print("Exchange battery:", counts["exchange"])
+    print("Failed battery:", counts["failed"])
     assert counts["healthy"] == 2
     assert counts["exchange"] == 3
     assert counts["failed"] == 1
